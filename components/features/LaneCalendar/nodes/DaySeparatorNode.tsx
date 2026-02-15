7cdbdb36cd52004a5c0e906f3787a54912980c2d
chris_niklas <christian.niklas@posteo.de>
2026-02-15 08:20:39 +0100

    feat(calendar): add DaySeparatorNode midnight line

---
+++ components/features/LaneCalendar/nodes/DaySeparatorNode.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { memo } from "react";
+import { type NodeProps } from "@xyflow/react";
+import { DAY_SEPARATOR_WIDTH } from "../utils/constants";
+
+export type DaySeparatorData = {
+  label: string;  // e.g. "Fri 26 Jun"
+  height: number; // total canvas height in px
+};
+
+function DaySeparatorNodeComponent({ data }: NodeProps) {
+  const { label, height } = data as DaySeparatorData;
+
+  return (
+    <div
+      style={{
+        width: `${DAY_SEPARATOR_WIDTH}px`,
+        height: `${height}px`,
+        position: "relative",
+        pointerEvents: "none",
+      }}
+    >
+      {/* Vertical line */}
+      <div
+        style={{
+          width: "1px",
+          height: "100%",
+          backgroundColor: "rgba(0,0,0,0.12)",
+        }}
+      />
+      {/* Day label */}
+      <div
+        className="absolute -top-6 left-2 text-xs font-medium text-gray-500 whitespace-nowrap"
+      >
+        {label}
+      </div>
+    </div>
+  );
+}
+
+export const DaySeparatorNode = memo(DaySeparatorNodeComponent);
