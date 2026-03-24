32a3f556795885fab4dd9bf08d5451a12a1604f1
chris_niklas <christian.niklas@posteo.de>
2026-03-24 17:55:29 +0100

    fix hydration warning. (standard)

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="antialiased bg-gray-50 text-gray-900">
+    <html lang="en" suppressHydrationWarning>
+      <body className="antialiased bg-gray-50 text-gray-900" suppressHydrationWarning>
         {children}
       </body>
     </html>
